6de0b368539a2667d7f9ac4d0fe64f9c7055c4da
Michal <michal@drontrzebownisko.pl>
2024-07-17 15:58:20 +0200

    Adapted the menu and header for larger displays and implemented appropriate animations.

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,11 +1,29 @@
+import { useState, useEffect } from "react";
 import classes from "./Header.module.scss";
 import logo from "../../public/logo.svg";
 import burgerMenu from "../assets/icons/burger-menu.svg";
 import Menu from "./Menu";
-import { useState } from "react";
 
 export default function Header() {
   const [menuOpen, setMenuOpen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 992) {
+        setMenuOpen(true);
+      } else {
+        setMenuOpen(false);
+      }
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
