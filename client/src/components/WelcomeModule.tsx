75719a53081289712b698b90a893563cc4ee93b1
Michal <michal@drontrzebownisko.pl>
2024-07-23 14:37:04 +0200

    Updated the WelcomeModule for styling.

--- client/src/components/WelcomeModule.tsx
+++ client/src/components/WelcomeModule.tsx
@@ -3,15 +3,14 @@ import classes from "./WelcomeModule.module.scss";
 export default function WelcomeModule() {
   return (
     <div className={classes.container}>
-      <h1>Podróże Szyte na Miarę!</h1>
       <div className={classes.text}>
         <p>
-          Jesteśmy ekspertami od niezapomnianych przygód! Czy marzysz o
-          ekscytującej wycieczce czy wyjątkowym obozie młodzieżowym - mamy coś
-          dla Ciebie, zarówno w Polsce, jak i za granicą. Każdą ofertę
-          dopasowujemy do Twoich upodobań i zainteresowań, bo wiemy, że
-          najlepsze podróże to te, które spełniają marzenia. Z nami przeżyjesz
-          podróż życia, skrojoną dokładnie na Twoją miarę!
+          <span>Jesteśmy ekspertami od niezapomnianych przygód!</span>
+          Czy marzysz o ekscytującej wycieczce czy wyjątkowym obozie
+          młodzieżowym - mamy coś dla Ciebie, zarówno w Polsce, jak i za
+          granicą. Każdą ofertę dopasowujemy do Twoich upodobań i zainteresowań,
+          bo wiemy, że najlepsze podróże to te, które spełniają marzenia. Z nami
+          przeżyjesz podróż życia, skrojoną dokładnie na Twoją miarę!
         </p>
       </div>
     </div>
