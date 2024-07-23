0ffb4600028d6e72f62f39e754728e774e63c367
Michal <michal@drontrzebownisko.pl>
2024-07-23 15:28:19 +0200

    Final styling adjustments for today.

--- client/src/components/Trip.tsx
+++ client/src/components/Trip.tsx
@@ -4,12 +4,15 @@ import tolo from "../assets/images/zimowisko.png";
 export default function Trip() {
   return (
     <div className={classes.trip}>
-      <img src={tolo} alt="business card" />
+      <div className={classes.image}>
+        <img src={tolo} alt="business card" />
+      </div>
       <div className={classes.overlay}>
         <span className={classes["overlay-text"]}>ZOBACZ WIĘCEJ</span>
       </div>
       <div className={classes["trip-title"]}>
         <h2>Tolo na Peloponezie</h2>
+        <p>Termin: 23.06. – 4.07.2024</p>
       </div>
     </div>
   );
