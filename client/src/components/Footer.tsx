c9c9f886ad2daae219d1ca39e8cee128a6ddc7c0
Michal <michal@drontrzebownisko.pl>
2024-08-03 16:25:48 +0200

    A footer component was added and styled for various screen resolutions.

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,23 @@
+import classes from "./Footer.module.scss";
+
+export default function Footer() {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer className={classes.footer}>
+      <div className={classes.company}>
+        <p>
+          &copy; 1999 - {currentYear} <span>AtlanticTravel.pl</span>
+        </p>
+      </div>
+      <div className={classes.author}>
+        <p>
+          Autor:{" "}
+          <a href="https://krudysz.pl/">
+            <span>Michał Krudysz</span>
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+}
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,12 +1,14 @@
 import classes from "./Home.module.scss";
 import WelcomeModule from "../components/WelcomeModule";
 import Trips from "../components/Trips";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
     <div className={classes.home}>
       <WelcomeModule />
       <Trips />
+      <Footer />
     </div>
   );
 }
