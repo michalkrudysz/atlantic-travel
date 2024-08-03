99d1d883485285692c75625fafdfec375d3582f0
Michal <michal@drontrzebownisko.pl>
2024-08-03 17:40:37 +0200

    Scrolling was disabled on touch devices when the mobile menu is displayed.

--- client/src/components/Menu.tsx
+++ client/src/components/Menu.tsx
@@ -1,8 +1,36 @@
+import { useEffect, useRef } from "react";
 import classes from "./Menu.module.scss";
 
 export default function Menu() {
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const menu = menuRef.current;
+    const preventWheel = (e: WheelEvent) => {
+      e.preventDefault();
+      e.stopPropagation();
+    };
+
+    const preventTouchMove = (e: TouchEvent) => {
+      e.preventDefault();
+      e.stopPropagation();
+    };
+
+    if (menu) {
+      menu.addEventListener("wheel", preventWheel, { passive: false });
+      menu.addEventListener("touchmove", preventTouchMove, { passive: false });
+
+      return () => {
+        if (menu) {
+          menu.removeEventListener("wheel", preventWheel);
+          menu.removeEventListener("touchmove", preventTouchMove);
+        }
+      };
+    }
+  }, []);
+
   return (
-    <div className={classes.menu}>
+    <div className={classes.menu} ref={menuRef}>
       <ul>
         <li>Nasza oferta</li>
         <li>Referencje</li>
--- client/src/pages/TripDetails.tsx
+++ client/src/pages/TripDetails.tsx
@@ -3,6 +3,7 @@ import classes from "./TripDetails.module.scss";
 import Title from "../components/TripDetails/Title";
 import TripDays from "../components/TripDetails/TripDays";
 import Services from "../components/Services";
+import Footer from "../components/Footer";
 
 export default function TripDetails() {
   const { tripTitleDetails } = useParams();
@@ -14,6 +15,7 @@ export default function TripDetails() {
         <Title />
         <TripDays />
         <Services />
+        <Footer />
       </div>
     </>
   );
