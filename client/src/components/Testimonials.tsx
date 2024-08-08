d928a8db4d9e0bc9223034fe30f91b20b049a156
Michal <michal@drontrzebownisko.pl>
2024-08-08 17:21:42 +0200

    A section for references was created and styled.

---
+++ client/src/components/Testimonials.tsx
@@ -0,0 +1,182 @@
+import classes from "./Testimonials.module.scss";
+import icon from "../../public/quote.png";
+
+export default function Testimonials() {
+  return (
+    <div className={classes.testimonials}>
+      <h1>Referencje</h1>
+      <div className={classes["testimonials-container"]}>
+        <div className={classes.container}>
+          <div className={classes.icon}>
+            <img src={icon} alt="cytat" />
+          </div>
+          <div className={classes.text}>
+            Wojewódzki Urząd Pracy w Rzeszowie rekomenduje Biuro
+            Usługowo-Turtystyczne "ATLANTIC" z siedzibą przy Placu Wolności 12 w
+            Rzeszowie jako solidne, godne zaufania biuro turystyczne
+            gwarantujące profesjonalizm i fachowość oferowanych usług.
+          </div>
+          <div className={classes.title}>
+            Wojewódzki Urząd Pracy w Rzeszowie
+          </div>
+          <div className={classes.button}>
+            <button>Zobacz referencję</button>
+          </div>
+        </div>
+        <div className={classes.container}>
+          <div className={classes.icon}>
+            <img src={icon} alt="cytat" />
+          </div>
+          <div className={classes.text}>
+            Wojewódzki Urząd Pracy w Rzeszowie rekomenduje Biuro
+            Usługowo-Turtystyczne "ATLANTIC" z siedzibą przy Placu Wolności 12 w
+            Rzeszowie jako solidne, godne zaufania biuro turystyczne
+            gwarantujące profesjonalizm i fachowość oferowanych usług.
+          </div>
+          <div className={classes.title}>
+            Wojewódzki Urząd Pracy w Rzeszowie
+          </div>
+          <div className={classes.button}>
+            <button>Zobacz referencję</button>
+          </div>
+        </div>
+        <div className={classes.container}>
+          <div className={classes.icon}>
+            <img src={icon} alt="cytat" />
+          </div>
+          <div className={classes.text}>
+            Wojewódzki Urząd Pracy w Rzeszowie rekomenduje Biuro
+            Usługowo-Turtystyczne "ATLANTIC" z siedzibą przy Placu Wolności 12 w
+            Rzeszowie jako solidne, godne zaufania biuro turystyczne
+            gwarantujące profesjonalizm i fachowość oferowanych usług.
+          </div>
+          <div className={classes.title}>
+            Wojewódzki Urząd Pracy w Rzeszowie
+          </div>
+          <div className={classes.button}>
+            <button>Zobacz referencję</button>
+          </div>
+        </div>
+        <div className={classes.container}>
+          <div className={classes.icon}>
+            <img src={icon} alt="cytat" />
+          </div>
+          <div className={classes.text}>
+            Wojewódzki Urząd Pracy w Rzeszowie rekomenduje Biuro
+            Usługowo-Turtystyczne "ATLANTIC" z siedzibą przy Placu Wolności 12 w
+            Rzeszowie jako solidne, godne zaufania biuro turystyczne
+            gwarantujące profesjonalizm i fachowość oferowanych usług.
+          </div>
+          <div className={classes.title}>
+            Wojewódzki Urząd Pracy w Rzeszowie
+          </div>
+          <div className={classes.button}>
+            <button>Zobacz referencję</button>
+          </div>
+        </div>
+        <div className={classes.container}>
+          <div className={classes.icon}>
+            <img src={icon} alt="cytat" />
+          </div>
+          <div className={classes.text}>
+            Wojewódzki Urząd Pracy w Rzeszowie rekomenduje Biuro
+            Usługowo-Turtystyczne "ATLANTIC" z siedzibą przy Placu Wolności 12 w
+            Rzeszowie jako solidne, godne zaufania biuro turystyczne
+            gwarantujące profesjonalizm i fachowość oferowanych usług.
+          </div>
+          <div className={classes.title}>
+            Wojewódzki Urząd Pracy w Rzeszowie
+          </div>
+          <div className={classes.button}>
+            <button>Zobacz referencję</button>
+          </div>
+        </div>
+        <div className={classes.container}>
+          <div className={classes.icon}>
+            <img src={icon} alt="cytat" />
+          </div>
+          <div className={classes.text}>
+            Wojewódzki Urząd Pracy w Rzeszowie rekomenduje Biuro
+            Usługowo-Turtystyczne "ATLANTIC" z siedzibą przy Placu Wolności 12 w
+            Rzeszowie jako solidne, godne zaufania biuro turystyczne
+            gwarantujące profesjonalizm i fachowość oferowanych usług.
+          </div>
+          <div className={classes.title}>
+            Wojewódzki Urząd Pracy w Rzeszowie
+          </div>
+          <div className={classes.button}>
+            <button>Zobacz referencję</button>
+          </div>
+        </div>
+        <div className={classes.container}>
+          <div className={classes.icon}>
+            <img src={icon} alt="cytat" />
+          </div>
+          <div className={classes.text}>
+            Wojewódzki Urząd Pracy w Rzeszowie rekomenduje Biuro
+            Usługowo-Turtystyczne "ATLANTIC" z siedzibą przy Placu Wolności 12 w
+            Rzeszowie jako solidne, godne zaufania biuro turystyczne
+            gwarantujące profesjonalizm i fachowość oferowanych usług.
+          </div>
+          <div className={classes.title}>
+            Wojewódzki Urząd Pracy w Rzeszowie
+          </div>
+          <div className={classes.button}>
+            <button>Zobacz referencję</button>
+          </div>
+        </div>
+        <div className={classes.container}>
+          <div className={classes.icon}>
+            <img src={icon} alt="cytat" />
+          </div>
+          <div className={classes.text}>
+            Wojewódzki Urząd Pracy w Rzeszowie rekomenduje Biuro
+            Usługowo-Turtystyczne "ATLANTIC" z siedzibą przy Placu Wolności 12 w
+            Rzeszowie jako solidne, godne zaufania biuro turystyczne
+            gwarantujące profesjonalizm i fachowość oferowanych usług.
+          </div>
+          <div className={classes.title}>
+            Wojewódzki Urząd Pracy w Rzeszowie
+          </div>
+          <div className={classes.button}>
+            <button>Zobacz referencję</button>
+          </div>
+        </div>
+        <div className={classes.container}>
+          <div className={classes.icon}>
+            <img src={icon} alt="cytat" />
+          </div>
+          <div className={classes.text}>
+            Wojewódzki Urząd Pracy w Rzeszowie rekomenduje Biuro
+            Usługowo-Turtystyczne "ATLANTIC" z siedzibą przy Placu Wolności 12 w
+            Rzeszowie jako solidne, godne zaufania biuro turystyczne
+            gwarantujące profesjonalizm i fachowość oferowanych usług.
+          </div>
+          <div className={classes.title}>
+            Wojewódzki Urząd Pracy w Rzeszowie
+          </div>
+          <div className={classes.button}>
+            <button>Zobacz referencję</button>
+          </div>
+        </div>
+        <div className={classes.container}>
+          <div className={classes.icon}>
+            <img src={icon} alt="cytat" />
+          </div>
+          <div className={classes.text}>
+            Wojewódzki Urząd Pracy w Rzeszowie rekomenduje Biuro
+            Usługowo-Turtystyczne "ATLANTIC" z siedzibą przy Placu Wolności 12 w
+            Rzeszowie jako solidne, godne zaufania biuro turystyczne
+            gwarantujące profesjonalizm i fachowość oferowanych usług.
+          </div>
+          <div className={classes.title}>
+            Wojewódzki Urząd Pracy w Rzeszowie
+          </div>
+          <div className={classes.button}>
+            <button>Zobacz referencję</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,6 +3,7 @@ import WelcomeModule from "../components/WelcomeModule";
 import Trips from "../components/Trips";
 import Footer from "../components/Footer";
 import SchoolTrips from "../components/SchoolTrips";
+import Testimonials from "../components/Testimonials";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <WelcomeModule />
       <Trips />
       <SchoolTrips />
+      <Testimonials />
       <Footer />
     </div>
   );
