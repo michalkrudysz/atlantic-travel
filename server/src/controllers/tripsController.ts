c0592cd60e5ab1acd24fd5f909b4d8efec391764
Michal <michal@drontrzebownisko.pl>
2024-08-09 14:29:55 +0200

    Added URL customization when retrieving addresses for files from the public folder, allowing relative paths to be stored in the database so that they are properly loaded regardless of the server.

--- server/src/controllers/tripsController.ts
+++ server/src/controllers/tripsController.ts
@@ -1,9 +1,24 @@
 import { Request, Response } from "express";
 import * as tripsService from "../services/tripsService";
+import { Trips } from "../models/trips";
 
-export const getAllTrips = async (req: Request, res: Response) => {
+function updateImageUrl(trips: Trips[], req: Request): void {
+  trips.forEach((trip) => {
+    if (trip.image) {
+      trip.image.image_url = `${req.protocol}://${req.get("host")}/${
+        trip.image.image_url
+      }`;
+    }
+  });
+}
+
+export const getAllTrips = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
   try {
-    const trips = await tripsService.getAllTrips();
+    let trips = await tripsService.getAllTrips();
+    updateImageUrl(trips, req);
     res.json(trips);
   } catch (error) {
     if (error instanceof Error) {
--- server/src/services/tripsService.ts
+++ server/src/services/tripsService.ts
@@ -1,13 +1,14 @@
 import { Trips } from "../models/trips";
 import { TripImages } from "../models/tripImages";
 
-export const getAllTrips = async () => {
+export const getAllTrips = async (): Promise<Trips[]> => {
   try {
     const trips = await Trips.findAll({
       attributes: ["trip_id", "title", "start_date", "end_date", "priority"],
       include: [
         {
           model: TripImages,
+          as: "image",
           attributes: ["image_url", "description"],
           required: false,
         },
