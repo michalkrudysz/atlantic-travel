1eae536a56156dc6f4eabc716808c756a5286f71
Michal <michal@drontrzebownisko.pl>
2024-08-28 14:16:14 +0200

    Developed and styled the login page.

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -3,7 +3,11 @@ import classes from "./Login.module.scss";
 export default function Login() {
   return (
     <div className={classes.login}>
-      <h1>TEST</h1>
+      <div className={classes["login-container"]}>
+        <h2>Wprowadź hasło</h2>
+        <input type="password" className={classes["login-input"]} />
+        <button className={classes["login-button"]}>Zaloguj się</button>
+      </div>
     </div>
   );
 }
