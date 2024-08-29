8e4cf7c08e6e4bec57bd9ada741885bcf909e319
Michal <michal@drontrzebownisko.pl>
2024-08-29 16:40:36 +0200

    Completed the styling and layout framework for the trip dashboard.

--- client/src/components/Dashboard/Trips.tsx
+++ client/src/components/Dashboard/Trips.tsx
@@ -8,24 +8,43 @@ export default function Trips() {
   return (
     <div className={classes.trips}>
       <div className={classes.trip}>
-        <div className={classes.title}>Trip Title</div>
+        <div className={classes.image}></div>
         <div className={classes["main-info"]}>
-          <div className={classes.image}></div>
           <div className={classes["title-info"]}>Paryż</div>
           <div className={classes["date-range"]}>
-            <div>2024-05-10</div>
-            <div>2024-08-11</div>
+            <div className={classes["date-label"]}>Data:</div>
+            <div className={classes["date-values"]}>
+              <span>2024-05-10</span>
+              <span>-</span>
+              <span>2024-08-11</span>
+            </div>
           </div>
-          <div className={classes.price}>1450.50</div>
-          <div className={classes["additional-costs"]}>200.00</div>
-          <div className={classes["description"]}>
-            100 zł obowiązkowa płatność na miejscu.
+          <div className={classes["price-info"]}>
+            <div className={classes["price-label"]}>Cena podstawowa:</div>
+            <div className={classes.price}>1450.50</div>
           </div>
-          <div className={classes["priority"]}>1</div>
+          <div className={classes["additional-costs-info"]}>
+            <div className={classes["additional-costs-label"]}>
+              Cena dodatkowa:
+            </div>
+            <div className={classes["additional-costs"]}>200.00</div>
+          </div>
+          <div className={classes["description-info"]}>
+            <div className={classes["description-label"]}>Dodatkowy opis:</div>
+            <div className={classes["description"]}>
+              obowiązkowa płatność na miejscu.
+            </div>
+          </div>
+          <div className={classes["priority-info"]}>
+            <div className={classes["priority-label"]}>
+              Priorytet wyświetlania:
+            </div>
+            <div className={classes["priority"]}>1</div>
+          </div>
         </div>
         <div className={classes["days"]}>
           <div className={classes["day"]}>
-            <div className={classes["day-number"]}>Numer dnia: 1</div>
+            <div className={classes["day-number"]}>Dzień: 1</div>
             <div className={classes["day-description"]}>
               Rozpoczęcie dnia od wizyty na najbardziej ikonicznym zabytku
               Paryża. Wjazd na górę, aby podziwiać panoramę miasta.Rozpoczęcie
@@ -36,7 +55,7 @@ export default function Trips() {
         </div>
         <div className={classes["included-excursions"]}>
           <div className={classes["name-of-excursion"]}>
-            Wycieczka wliczona:
+            Wycieczki wliczone:
           </div>
           <div className={classes["excursion"]}>
             Wycieczka do Salonik – kulinarnej stolicy Grecji z bizantyjskimi
@@ -48,13 +67,20 @@ export default function Trips() {
           </div>
         </div>
         <div className={classes["optional-excursions"]}>
-          Rejs statkiem na Skiathos – najpiękniejsza wyspa archipelagu Sporad,
-          42 euro/os
+          <div className={classes["name-of-excursion"]}>
+            Wycieczki fakultatywne
+          </div>
+          <div className={classes["optional-excursion"]}>
+            Rejs statkiem na Skiathos – najpiękniejsza wyspa archipelagu Sporad,
+            42 euro/os
+          </div>
         </div>
         <div className={classes["services"]}>
+          <div className={classes["name-of-service"]}>Świadczenia</div>
           <div className={classes["service"]}>7 noclegów w Grecji</div>
         </div>
         <div className={classes["trip-contact"]}>
+          <div className={classes["contact-info"]}>Dane kontaktowe</div>
           <div className={classes.phone}>17 852 66 76</div>
           <div className={classes.phone}>510 991 590</div>
           <div className={classes.email}>info@atlantictravel.pl</div>
