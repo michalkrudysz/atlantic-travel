93e9ebf8786b799ce40a209a677f0ba8ef739370
Michal <michal@drontrzebownisko.pl>
2024-09-04 13:30:15 +0200

    Classes were added and styling was updated for modifying contact information related to the trip.

--- client/src/components/Dashboard/ContactTrip.tsx
+++ client/src/components/Dashboard/ContactTrip.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import classes from "./ContactTrip.module.scss";
-import { useModifyContactTrip } from "../../hooks/useModifyContactTrip"; // Ensure this path is correct
-
+import { useModifyContactTrip } from "../../hooks/useModifyContactTrip";
 type Contact = {
   phone1?: string;
   phone2?: string;
@@ -21,7 +20,7 @@ type ContactTripProps = {
 const ContactTrip: React.FC<ContactTripProps> = ({ tripContacts, tripId }) => {
   const [isEditing, setIsEditing] = useState(false);
   const [formData, setFormData] = useState<Contact>(tripContacts[0] || {});
-  const [contacts, setContacts] = useState<Contact[]>(tripContacts); // New state for tripContacts
+  const [contacts, setContacts] = useState<Contact[]>(tripContacts);
   const modifyContactTrip = useModifyContactTrip();
 
   const toggleEdit = () => {
@@ -58,113 +57,141 @@ const ContactTrip: React.FC<ContactTripProps> = ({ tripContacts, tripId }) => {
 
   return (
     <div className={classes["trip-contact"]}>
-      <div className={classes["contact-info"]}>Dane kontaktowe:</div>
       {!isEditing ? (
-        <div className={classes["contact-details"]}>
-          {contacts.map((contact, index) => (
-            <div key={index}>
-              {contact.phone1 && (
-                <div
-                  className={classes.phone}
-                >{`Telefon: ${contact.phone1}`}</div>
-              )}
-              {contact.phone2 && (
-                <div
-                  className={classes.phone}
-                >{`Telefon: ${contact.phone2}`}</div>
-              )}
-              {contact.phone3 && (
-                <div
-                  className={classes.phone}
-                >{`Telefon: ${contact.phone3}`}</div>
-              )}
-              {contact.email1 && (
-                <div
-                  className={classes.email}
-                >{`Email: ${contact.email1}`}</div>
-              )}
-              {contact.email2 && (
-                <div
-                  className={classes.email}
-                >{`Email: ${contact.email2}`}</div>
-              )}
-              {contact.payment_instructions && (
-                <div
-                  className={classes["payment-instructions"]}
-                >{`Instrukcje płatności: ${contact.payment_instructions}`}</div>
-              )}
-              {contact.additional_description && (
-                <div
-                  className={classes["additional-description"]}
-                >{`Dodatkowe informacje: ${contact.additional_description}`}</div>
-              )}
-              {contact.payment_reference && (
-                <div
-                  className={classes["payment-reference"]}
-                >{`Referencje płatności: ${contact.payment_reference}`}</div>
-              )}
-            </div>
-          ))}
+        <>
+          <div className={classes["contact-info"]}>Dane kontaktowe:</div>
+          <div className={classes["contact-details"]}>
+            {contacts.map((contact, index) => (
+              <div key={index}>
+                {contact.phone1 && (
+                  <div className={classes.phone}>
+                    <span className={classes.label}>Telefon 1:</span>
+                    <span>{contact.phone1}</span>
+                  </div>
+                )}
+                {contact.phone2 && (
+                  <div className={classes.phone}>
+                    <span className={classes.label}>Telefon 2:</span>
+                    <span>{contact.phone2}</span>
+                  </div>
+                )}
+                {contact.phone3 && (
+                  <div className={classes.phone}>
+                    <span className={classes.label}>Telefon 3:</span>
+                    <span>{contact.phone3}</span>
+                  </div>
+                )}
+                {contact.email1 && (
+                  <div className={classes.email}>
+                    <span className={classes.label}>Email 1:</span>
+                    <span>{contact.email1}</span>
+                  </div>
+                )}
+                {contact.email2 && (
+                  <div className={classes.email}>
+                    <span className={classes.label}>Email 2:</span>
+                    <span>{contact.email2}</span>
+                  </div>
+                )}
+                {contact.payment_instructions && (
+                  <div className={classes["payment-instructions"]}>
+                    <span className={classes.label}>Instrukcje płatności:</span>
+                    <div>{contact.payment_instructions}</div>
+                  </div>
+                )}
+                {contact.additional_description && (
+                  <div className={classes["additional-description"]}>
+                    <span className={classes.label}>Dodatkowe informacje:</span>
+                    <div>{contact.additional_description}</div>
+                  </div>
+                )}
+                {contact.payment_reference && (
+                  <div className={classes["payment-reference"]}>
+                    <span className={classes.label}>Referencje płatności:</span>
+                    <span>{contact.payment_reference}</span>
+                  </div>
+                )}
+              </div>
+            ))}
+          </div>
           <div className={classes["actions"]}>
             <button onClick={toggleEdit}>Edytuj</button>
           </div>
-        </div>
+        </>
       ) : (
-        <form onSubmit={handleSubmit} className={classes["contact-details"]}>
-          <input
-            type="text"
-            name="phone1"
-            value={formData.phone1 || ""}
-            onChange={handleChange}
-            placeholder="Telefon 1"
-          />
-          <input
-            type="text"
-            name="phone2"
-            value={formData.phone2 || ""}
-            onChange={handleChange}
-            placeholder="Telefon 2"
-          />
-          <input
-            type="text"
-            name="phone3"
-            value={formData.phone3 || ""}
-            onChange={handleChange}
-            placeholder="Telefon 3"
-          />
-          <input
-            type="email"
-            name="email1"
-            value={formData.email1 || ""}
-            onChange={handleChange}
-            placeholder="Email 1"
-          />
-          <input
-            type="email"
-            name="email2"
-            value={formData.email2 || ""}
-            onChange={handleChange}
-            placeholder="Email 2"
-          />
-          <textarea
-            name="payment_instructions"
-            value={formData.payment_instructions || ""}
-            onChange={handleChange}
-            placeholder="Instrukcje płatności"
-          ></textarea>
-          <textarea
-            name="additional_description"
-            value={formData.additional_description || ""}
-            onChange={handleChange}
-            placeholder="Dodatkowe informacje"
-          ></textarea>
-          <input
-            type="text"
-            name="payment_reference"
-            value={formData.payment_reference || ""}
-            onChange={handleChange}
-            placeholder="Referencje płatności"
-          />
+        <form onSubmit={handleSubmit}>
+          <div className={classes["form-field"]}>
+            <label htmlFor="phone1">Telefon 1:</label>
+            <input
+              type="text"
+              name="phone1"
+              value={formData.phone1 || ""}
+              onChange={handleChange}
+            />
+          </div>
+          <div className={classes["form-field"]}>
+            <label htmlFor="phone2">Telefon 2:</label>
+            <input
+              type="text"
+              name="phone2"
+              value={formData.phone2 || ""}
+              onChange={handleChange}
+            />
+          </div>
+          <div className={classes["form-field"]}>
+            <label htmlFor="phone3">Telefon 3:</label>
+            <input
+              type="text"
+              name="phone3"
+              value={formData.phone3 || ""}
+              onChange={handleChange}
+            />
+          </div>
+          <div className={classes["form-field"]}>
+            <label htmlFor="email1">Email 1:</label>
+            <input
+              type="email"
+              name="email1"
+              value={formData.email1 || ""}
+              onChange={handleChange}
+            />
+          </div>
+          <div className={classes["form-field"]}>
+            <label htmlFor="email2">Email 2:</label>
+            <input
+              type="email"
+              name="email2"
+              value={formData.email2 || ""}
+              onChange={handleChange}
+            />
+          </div>
+          <div className={classes["form-field"]}>
+            <label htmlFor="payment_instructions">Instrukcje płatności:</label>
+            <textarea
+              name="payment_instructions"
+              value={formData.payment_instructions || ""}
+              onChange={handleChange}
+            ></textarea>
+          </div>
+          <div className={classes["form-field"]}>
+            <label htmlFor="additional_description">
+              Dodatkowe informacje:
+            </label>
+            <textarea
+              name="additional_description"
+              value={formData.additional_description || ""}
+              onChange={handleChange}
+            ></textarea>
+          </div>
+          <div className={classes["form-field"]}>
+            <label htmlFor="payment_reference">Referencje płatności:</label>
+            <input
+              type="text"
+              name="payment_reference"
+              value={formData.payment_reference || ""}
+              onChange={handleChange}
+            />
+          </div>
           <div className={classes["actions"]}>
             <button type="submit">Zapisz</button>
             <button type="button" onClick={toggleEdit}>
